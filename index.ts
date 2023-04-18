a14494fed3aab3dea3d36e7b4852751685b60362
Komodo123 <ethananders97@gmail.com>
2023-04-18 15:59:22 -0700

    Initial upload

---
+++ index.ts
@@ -0,0 +1,92 @@
+export const WILDCARD = "*";
+export const NEGATION = "-";
+
+export type Wildcard = typeof WILDCARD;
+export type Negation = typeof NEGATION;
+
+type Primitive = bigint | boolean | null | number | string | undefined;
+
+export type Pattern<T extends Primitive> =
+  | Wildcard
+  | `${Negation}${Wildcard}`
+  | `${Negation}${T}`
+  | T
+  | RegExp
+  | (Wildcard | T | RegExp)[];
+
+export type IncludeOptions = {
+  allowWildNeedle?: boolean;
+  allowWildHaystack?: boolean;
+};
+
+export function includes<T extends Primitive>(
+  needle: Pattern<T>,
+  haystack: Pattern<T>,
+  options: IncludeOptions = {
+    allowWildNeedle: true,
+    allowWildHaystack: true,
+  }
+): boolean {
+  if (Array.isArray(needle)) {
+    // e.x. needle [], haystack = "*"
+    if (
+      haystack === WILDCARD &&
+      options.allowWildHaystack &&
+      needle.length <= 0
+    ) {
+      return true;
+    }
+
+    for (const item of needle) {
+      if (!includes(item, haystack, options)) {
+        return false;
+      }
+    }
+
+    return true;
+  } else if (Array.isArray(haystack)) {
+    // e.x. needle = "*", haystack = []
+    if (
+      needle === WILDCARD &&
+      options.allowWildNeedle &&
+      haystack.length <= 0
+    ) {
+      return true;
+    }
+
+    for (const pattern of haystack) {
+      if (typeof pattern === "string") {
+        if (
+          pattern[0] === NEGATION &&
+          includes(needle, <Pattern<T>>pattern.substring(1), options)
+        ) {
+          return false;
+        }
+      }
+    }
+
+    for (const pattern of haystack) {
+      if (includes(needle, pattern, options)) {
+        return true;
+      }
+    }
+  } else if (haystack === WILDCARD && options.allowWildHaystack) {
+    return true;
+  } else if (
+    needle === WILDCARD &&
+    options.allowWildNeedle &&
+    haystack !== NEGATION + WILDCARD
+  ) {
+    return true;
+  }
+
+  if (haystack instanceof RegExp) {
+    return haystack.test(`${needle}`);
+  }
+
+  if (needle instanceof RegExp) {
+    return needle.test(`${haystack}`);
+  }
+
+  return haystack === needle;
+}
---
+++ test.ts
@@ -0,0 +1,42 @@
+import assert from "node:assert";
+import { includes } from "./index";
+
+assert(includes("google.com", "*") === true);
+assert(includes("google.com", "google.com") === true);
+assert(includes("google.com", ["amazon.com", "-google.com"]) === false);
+assert(includes("google.com", ["*", "-google.com"]) === false);
+assert(includes("google.com", ["-*", "google.com"]) === false);
+assert(includes("*", ["amazon.com"]) === true);
+assert(includes("*", ["-*", "google.com"]) === false);
+assert(includes("*", "-*") === false);
+assert(includes(["*"], []) === true);
+assert(includes([], ["*"]) === true);
+assert(includes("*", "amazon.com") === true);
+assert(includes(["google.com", "amazon.com"], ["amazon.com"]) === false);
+assert(includes(["google.com", "amazon.com"], "*") === true);
+assert(
+  includes(
+    ["google.com", "amazon.com"],
+    ["apple.com", "amazon.com", "google.com"]
+  ) === true
+);
+assert(
+  includes(
+    ["google.com", "amazon.com"],
+    ["apple.com", "amazon.com", "-google.com"]
+  ) === false
+);
+assert(includes(/.*/, "google.com") === true);
+assert(includes(/google/, ["google.com", "apple.com"]) === true);
+assert(includes(/amazon/, ["google.com", "apple.com"]) === false);
+assert(includes([/apple/, /amazon/], ["google.com", "apple.com"]) === false);
+assert(includes("google.com", [/.*/]) === true);
+assert(includes("google.com", /amazon/) === false);
+assert(includes("google.com", [/google/, "-google.com"]) === false);
+assert(includes("amazon.com", [/apple/, "-google.com", "*"]) === true);
+assert(
+  includes(
+    ["amazon.com", "apple.com", "google.com"],
+    [/apple/, "-google.com", "*"]
+  ) === false
+);
